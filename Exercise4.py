a9df508ed15a81091f3bff6ef28e30295c7bc1b3
MchllD <mich11dimafelix@gmail.com>
2024-01-25 23:10:47 +0800

    pseudocode

---
+++ Exercise4.py
@@ -0,0 +1,17 @@
+# Exercise 4: Remove first n characters from a string
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# pseudocode
+
+# Function to remove characters from a string
+    # Set result to the substring starting from index n to the end of the string
+    # Return the modified string
+   
+
+# Print a message indicating that characters are being removed from a string
+
+# Set the input string and the number of characters to remove
+
+# Call the remove_chars function with the input string and the number of characters to remove
+
+# Print the original string and the string after removing the specified number of characters
